ab01fcc79a32a7b1216f1395db4826663b8bc950
Rodion Efremov <coderodd3@yandex.com>
2025-11-29 17:54:57 +0200

    ...

--- frequency_distribution_builder.c
+++ frequency_distribution_builder.c
@@ -4,14 +4,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <pthread.h>
-#endif
 
 // File read buffer size in bytes, 64 KiB:
-#define BUFFER_SIZE (1024 * 4)
+#define BUFFER_SIZE (1024 * 64)
 
 static size_t file_size(const char *const file_name) {
     FILE* file = fopen(file_name, "rb");
@@ -27,22 +22,39 @@ FrequencyDistribution* frequency_distribution_builder_build(const char *const fi
     const size_t data_size = file_size(file_name);
 
     FrequencyDistribution* distribution = malloc(sizeof *distribution);
-    frequency_distribution_init(distribution);
+    
+    ABORT_ON(distribution == NULL)
 
-    uint8_t* buffer = malloc(data_size);
-    ABORT_ON(buffer == NULL)
+    frequency_distribution_init(distribution);
 
     FILE* file = fopen(file_name, "rb");
     ABORT_ON(file == NULL)
-    ABORT_ON(setvbuf(file, (char*) buffer, _IOFBF, data_size) != 0)
+    uint8_t* buffer = malloc(BUFFER_SIZE);
+    ABORT_ON(buffer == NULL)
+    ABORT_ON(setvbuf(file, (char*) buffer, _IOFBF, BUFFER_SIZE) != 0)
 
-    const size_t bytes_read = fread(buffer, sizeof(uint8_t), data_size, file);
-        
-    ABORT_ON(bytes_read != data_size)
+    size_t bytes_remaining = data_size;
 
-    for (size_t i = 0; i < data_size; ++i) {
-        frequency_distribution_increment(distribution, (uint8_t) buffer[i]);
+    while (bytes_remaining > 0) {
+
+        const size_t bytes_to_read = 
+            (bytes_remaining > BUFFER_SIZE ? BUFFER_SIZE : bytes_remaining);
+
+        const size_t bytes_read = fread(buffer, 
+                                        sizeof(uint8_t), 
+                                        bytes_to_read, 
+                                        file);
+
+        ABORT_ON(bytes_read != bytes_to_read)
+        ABORT_ON(ferror(file) != 0)
+
+        for (size_t i = 0; i < bytes_read; ++i) {
+            frequency_distribution_increment(distribution, buffer[i]);
+        }
+
+        bytes_remaining -= bytes_read;
     }
 
+    fclose(file);
     return distribution;
 }
